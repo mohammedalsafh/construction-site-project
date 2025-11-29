77bb2f1f541243cfec3e3cdfd234251f1621dcff
abdulkaderburhan <abdulkaderburhan455@gmail.com>
2025-11-29 12:36:07 +0300

    Update HomeController.cs

--- CPIS-358-project/Controllers/HomeController.cs
+++ CPIS-358-project/Controllers/HomeController.cs
@@ -16,49 +16,48 @@ namespace CPIS_358_project.Controllers
             return View();
         }
 
-        // Corresponds to product info.html
+       
         public IActionResult ProductInfo()
         {
             return View();
         }
 
-        // Corresponds to Services.html
+        
         public IActionResult Services()
         {
             return View();
         }
 
-        // Corresponds to contact.html
+        
         public IActionResult Contact()
         {
             return View();
         }
 
-        // Corresponds to log-in.html
+        
         public IActionResult Login()
         {
             return View();
         }
 
-        // Corresponds to sign-up.html (You didn't upload this, but I see it linked)
         public IActionResult SignUp()
         {
             return View();
         }
 
-        // Corresponds to forgotPass.html
+        
         public IActionResult ForgotPassword()
         {
             return View();
         }
 
-        // Corresponds to storePage.html (You didn't upload this, but it is linked)
+       
         public IActionResult Store()
         {
             return View();
         }
 
-        // Corresponds to payment.html
+        
         public IActionResult Payment()
         {
             return View();
