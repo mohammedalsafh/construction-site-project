8e8ec0754a3cbd4d1289150538b5a563428b6937
abdulkader <abdulkaderburhan455@gmail.com>
2025-11-29 13:12:40 +0300

    nothing new

--- CPIS-358-project/Controllers/HomeController.cs
+++ CPIS-358-project/Controllers/HomeController.cs
@@ -6,41 +6,42 @@ namespace CPIS_358_project.Controllers
 {
     public class HomeController : Controller
     {
+        // Corresponds to HomePage.html
         public IActionResult Index()
         {
             return View();
         }
 
+        // Corresponds to about.html
         public IActionResult About()
         {
             return View();
         }
 
-        
+        // Corresponds to product info.html
         public IActionResult ProductInfo()
         {
             return View();
         }
-
-        
+        // Corresponds to Services.html
         public IActionResult Services()
         {
             return View();
         }
 
-        
+        // Corresponds to contact.html
         public IActionResult Contact()
         {
             return View();
         }
 
-       
+        // Corresponds to log-in.html
         public IActionResult Login()
         {
             return View();
         }
 
-        
+        // Corresponds to sign-up.html (You didn't upload this, but I see it linked)
         public IActionResult SignUp()
         {
             return View();
@@ -53,20 +54,21 @@ namespace CPIS_358_project.Controllers
                 return View();
             }
 
+
             return RedirectToAction(nameof(Login));
         }
-
-        
+        // Corresponds to forgotPass.html
         public IActionResult ForgotPassword()
         {
             return View();
         }
 
+        // Corresponds to storePage.html (You didn't upload this, but it is linked)
         public IActionResult Store()
         {
             return View();
         }
-
+        // Corresponds to payment.html
         public IActionResult Payment()
         {
             return View();
