f24793b2b0298fb9af399bb247af72e85d867d7a
abdulkader <abdulkaderburhan455@gmail.com>
2025-12-02 13:42:55 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,78 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Identity;
+using CPIS_358_project.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CPIS_358_project.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+       
+        public IActionResult ProductInfo()
+        {
+            return View();
+        }
+
+        
+        public IActionResult Services()
+        {
+            return View();
+        }
+
+        
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SignUp(User user)
+        {
+            if (String.IsNullOrEmpty(user.FullName) || String.IsNullOrEmpty(user.UserE))
+            {
+                return View();
+            }
+
+
+            return RedirectToAction(nameof(Store));
+        }
+        
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+
+       
+        public IActionResult Store()
+        {
+            return View();
+        }
+
+        
+        public IActionResult Payment()
+        {
+            return View();
+        }
+    }
+}
