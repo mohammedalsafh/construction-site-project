b263e332de3c731107f58af1875d521d59b039d6
Admin <Admin@DESKTOP-3PIB3QG>
2025-12-03 19:47:10 +0300

    Add project files.

---
+++ Controllers/ServicesController.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CPIS_358_project.Models;
+using CPIS_358_project.Data;
+
+namespace CPIS_358_project.Controllers
+{
+    public class ServicesController : Controller
+    {
+        private readonly CPIS_358_projectContext _context;
+
+        public ServicesController(CPIS_358_projectContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Services
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Services.ToListAsync());
+        }
+
+        // GET: Services/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var services = await _context.Services
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (services == null)
+            {
+                return NotFound();
+            }
+
+            return View(services);
+        }
+
+        // GET: Services/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Services/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Name,Profession,yearsOfExperience")] Services services)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(services);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(services);
+        }
+
+        // GET: Services/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var services = await _context.Services.FindAsync(id);
+            if (services == null)
+            {
+                return NotFound();
+            }
+            return View(services);
+        }
+
+        // POST: Services/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Name,Profession,yearsOfExperience")] Services services)
+        {
+            if (id != services.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(services);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ServicesExists(services.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(services);
+        }
+
+        // GET: Services/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var services = await _context.Services
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (services == null)
+            {
+                return NotFound();
+            }
+
+            return View(services);
+        }
+
+        // POST: Services/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var services = await _context.Services.FindAsync(id);
+            if (services != null)
+            {
+                _context.Services.Remove(services);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ServicesExists(int id)
+        {
+            return _context.Services.Any(e => e.ID == id);
+        }
+
+        public async Task<IActionResult> SearchForm()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> SearchResults(string SearchString)
+        {
+            if(_context.Services == null)
+            {
+                return Problem("Entity set 'CPIS-358-project' is null");
+            }
+            var filteredService = await _context.Services.Where(x => x.Profession.Contains(SearchString)).ToListAsync();
+            return View("Index",filteredService);
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+namespace CPIS_358_project.Models
+{
+    public class User : IdentityUser//creating user class it connects to IdentityUser so we get all the built in stuff like email and password automatically
+    {
+        public required string FullName { get; set; }//we added this variable because the default user doesnt have a name field
+        //required means it has to be filled
+
+    }
+}
