e57bb339f18c2d04bcb0b8a29be41c4dcd207471
abdulkader <abdulkaderburhan455@gmail.com>
2025-12-03 12:27:26 +0300

    Add project files.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc; 
+
+namespace CPIS_358_project.Controllers 
+{
+    //this controller is made so that we handle everything related to the user account page 
+    public class AccountController : Controller
+    {
+        //when we call this it return the index view for the account
+        public IActionResult Index()//we create the function for the index page
+        {
+            return View();//this will show the actual page to the user
+        }
+    }
+}
---
+++ Data/CPIS_358_projectContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using CPIS_358_project.Models;
+namespace CPIS_358_project.Data;
+
+public class CPIS_358_projectContext : IdentityDbContext<User>
+{
+    public CPIS_358_projectContext(DbContextOptions<CPIS_358_projectContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+
+public DbSet<Services> Services { get; set; } = default!;
+}
