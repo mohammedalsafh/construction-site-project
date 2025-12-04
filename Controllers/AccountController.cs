519647225888a95154db89610a004d92d2066272
abdulkader <abdulkaderburhan455@gmail.com>
2025-12-04 13:06:11 +0300

    Add project files.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc; 
+
+namespace CPIS_358_project.Controllers 
+{
+    //this controller is made so that we handle everything related to the user account page 
+    public class AccountController : Controller
+    {
+        //when we call this it return the index view for the account
+        public IActionResult Index()//we create the function for the index page
+        {
+            ViewBag.sessionID = HttpContext.Session.GetString("id");
+            ViewBag.sessionUserName = HttpContext.Session.GetString("username");
+            return View();//this will show the actual page to the user
+        }
+    }
+}
---
+++ Models/Services.cs
@@ -0,0 +1,22 @@
+﻿namespace CPIS_358_project.Models
+{
+    public class Services
+    {
+        public int ID { get; set; }
+        public string Name 
+        { get; set; }
+        required
+        
+        public string Profession
+        { get; set; }
+        required
+        public int yearsOfExperience
+        { get; set; }
+
+
+        public Services()
+        {
+
+        }
+    }
+}
